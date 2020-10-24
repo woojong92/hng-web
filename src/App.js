77e04dd7a35b0288a1c5edcdb30917340cfe989e
woojong92 <woojong92@gmail.com>
2020-10-24 13:33:03 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
+import hng from "hangeul-nickname-generator";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #fdd981;
+`;
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  padding: 2rem;
+  // height: 15rem;
+  border: 1px solid #fcc02d;
+  border-radius: 15px;
+  background-color: #fcc02d;
+`;
 
 function App() {
+  const [nickname, setNickname] = useState(" ");
+
+  const onClick = () => {
+    setNickname(hng());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Wrapper>
+      <Box>
+        <text style={{ fontSize: "100px", marginBottom: "1rem" }}>🐥</text>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            marginBottom: "1.5rem",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <text style={{ marginRight: "1rem", fontSize: "25px" }}>
+            지금 당장 한글 닉네임을 만들어보세요.
+          </text>
+          <div
+            style={{
+              padding: "0.5rem",
+              backgroundColor: "#F08A00",
+              borderRadius: "8px",
+              cursor: "pointer",
+            }}
+            onClick={onClick}
+          >
+            <text style={{ fontSize: "20" }}>Click!!</text>
+          </div>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#e5e5e5",
+            width: "100%",
+            height: "40px",
+            borderRadius: "15px",
+          }}
+        >
+          <text style={{ fontSize: "23px", color: "#F08A00" }}>{nickname}</text>
+        </div>
+      </Box>
+    </Wrapper>
   );
 }
 
